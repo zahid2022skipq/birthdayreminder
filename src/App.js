219522e63c9b93323c442903ca1fab0b1dbbafb7
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-20 13:52:57 +0500

    Static UI done Birthday app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 Birthdays Today</h3>
+        <List />
+        <button onClick={() => console.log("Uou clicked me")}>Clear Me</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
